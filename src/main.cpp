5db860b0cb5d8b65b6b6cd9612de758e6f5ad074
JCE98 <josiah@raecodesign.com>
2023-09-21 22:32:20 +0000

    Reorganized codebase, additional comments

---
+++ src/main.cpp
@@ -0,0 +1,37 @@
+#include<stdlib.h>
+#include<iostream>
+#include<vector>
+#include<board.h>
+#include<player.h>
+using namespace std;
+
+int main()
+{
+    //Start Game
+    Board gameBoard;
+    Player player;
+    gameBoard.displayHeader();
+    gameBoard.displayBoard();
+
+    //Control-Update-Render Loop
+    while (gameBoard.gameStatus() == false)
+    {
+        player.switchPlayer();
+        int move = player.takeTurn(gameBoard.getBoardState());
+        gameBoard.updateBoard(move,player);
+        gameBoard.displayHeader();
+        gameBoard.displayBoard();
+    }
+
+    //End Game
+    gameBoard.displayOutcome(player);
+    return 0;
+};
+
+/*
+[1] 'C++ Tic Tac Toe Game Project', STechies.com, 13 Nov 2020, https://www.stechies.com/tic-tac-toe-cpp/
+[2] 'Introduction to Evaluation Function of Minimax Algorithm in Game Theory', GeeksForGeeks.org,
+https://www.geeksforgeeks.org/introduction-to-evaluation-function-of-minimax-algorithm-in-game-theory/
+[3] 'Finding optimal move in Tic-Tac-Toe using Minimax Algorithm in Game Theory', GeeksForGeeks.org,
+https://www.geeksforgeeks.org/finding-optimal-move-in-tic-tac-toe-using-minimax-algorithm-in-game-theory/
+*/
